70f277b9096ef052d8ac42914f3fc52b712225d7
andresokol <sokolov-as-i@yandex.ru>
2022-02-04 23:04:32 +0400

    add conftest

--- tests/test_timedata.py
+++ tests/test_timedata.py
@@ -1,8 +1,14 @@
 import pytest
 
-from .timedata import Timedata
+from timedata import Timedata
 
 
-@pytest.parametrize("input_string, expected_result", ["2:30:30", Timedata(2, 30, 30)])
+@pytest.mark.parametrize(
+    "input_string, expected_result",
+    [
+        ["2:30:30", Timedata(2, 30, 30)],
+    ],
+)
 def test_from_string(input_string, expected_result):
-    assert Timedata.from_string(input_string) == expected_result
+    result = Timedata.from_string(input_string)
+    assert result == expected_result
