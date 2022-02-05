b1f0ae12e46612a2fae14e5f57037908cae3b19e
andresokol <sokolov-as-i@yandex.ru>
2022-02-05 12:29:54 +0400

    move limit to class attrs

--- timedata.py
+++ timedata.py
@@ -2,12 +2,14 @@ import typing as tp
 
 
 class Timedata:
+    LIMIT: int = 60
+
     def __init__(self, hours: int, minutes: int, seconds: int):
         if not hours >= 0:
             raise ValueError(f"hours should not less than 0, got {hours}")
-        if not 0 <= minutes < 60:
+        if not 0 <= minutes < self.LIMIT:
             raise ValueError(f"minutes should be between 0 and 59, got {minutes}")
-        if not 0 <= seconds < 60:
+        if not 0 <= seconds < self.LIMIT:
             raise ValueError(f"seconds should be between 0 and 59, got {seconds}")
 
         self.hours = hours
@@ -22,7 +24,7 @@ class Timedata:
         parts = sample.split(":")
         parts_formatted = [int(part) for part in parts]
 
-        return cls(*parts_formatted)  # 🤔🤔🤔
+        return cls(*parts_formatted)
 
     def __repr__(self) -> str:
         return f"<Timedata {self.to_string()}>"
@@ -30,18 +32,19 @@ class Timedata:
     def __str__(self) -> str:
         return self.to_string()
 
-    @staticmethod
+    @classmethod
     def _normalize(
+        cls,
         hours: int,
         minutes: int,
         seconds: int,
     ) -> tp.Tuple[int, int, int]:
-        if seconds >= 60:
-            minutes += seconds // 60
-            seconds = seconds % 60
-        if minutes >= 60:
-            hours += minutes // 60
-            minutes = minutes % 60
+        if seconds >= cls.LIMIT:
+            minutes += seconds // cls.LIMIT
+            seconds = seconds % cls.LIMIT
+        if minutes >= cls.LIMIT:
+            hours += minutes // cls.LIMIT
+            minutes = minutes % cls.LIMIT
 
         return hours, minutes, seconds
 
@@ -50,13 +53,7 @@ class Timedata:
         minutes = self.minutes + other.minutes
         hours = self.hours + other.hours
 
-        return Timedata(*self._normalize(hours, minutes, seconds))
-
-    def to_tuple(self) -> tp.Tuple[int, int, int]:
-        return (self.hours, self.minutes, self.seconds)
-
-    def __eq__(self, other) -> bool:
-        return self.to_tuple() == other.to_tuple()
+        return self.__class__(*self._normalize(hours, minutes, seconds))
 
 
 if __name__ == "__main__":
